3ca783e123f9bd4892b034938711fd222bdec5cb
Ronaldo Kereh <kerehchannel@gmail.com>
2023-08-27 10:55:24 +0800

    fix: heading elements are not in a sequentially-descending order

--- src/components/products/product-card.tsx
+++ src/components/products/product-card.tsx
@@ -29,9 +29,9 @@ export default function ProductCard({ title, img, link }: IProductCard) {
         />
       </AspectRatio>
       <div className="absolute inset-0 z-10 flex items-center justify-center">
-        <h3 className="text-3xl font-medium capitalize text-slate-100 md:text-2xl">
+        <h1 className="text-3xl font-medium capitalize text-slate-100 md:text-2xl">
           {title}
-        </h3>
+        </h1>
       </div>
     </div>
   )
