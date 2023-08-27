e5b6165b84593ca673e6f35f708d52451f3de273
Ronaldo Kereh <kerehchannel@gmail.com>
2023-08-27 16:49:36 +0800

    update: metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,15 +2,48 @@ import { Metadata } from "next"
 import Image from "next/image"
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://mtm-kereh.vercel.app/"),
   title: {
-    default: "About Us",
-    template: "Manguni Teknik Multimedia - %s"
+    default: "Manguni Teknik Multimedia",
+    template: `%s - Manguni Teknik Multimedia`,
   },
-  description: 'About Us - Manguni Teknik Multimedia',
+  description: "kami adalah tim kreatif yang berdedikasi tinggi dalam menghadirkan visual dan pengalaman digital yang luar biasa. Sebagai perusahaan yang berfokus pada videography, fotografi, dan pengembangan web, kami menggabungkan keahlian teknis dengan imajinasi artistik untuk menciptakan konten yang menarik dan solusi digital yang inovatif.",
+  keywords: [
+    "Photography",
+    "Videography",
+    "Web Developer",
+    "Manguni",
+    "Multimedia",
+    "Web Developer",
+  ],
+  authors: [
+    {
+      name: "kereh",
+      url: "https://github.com/kereh",
+    },
+  ],
+  creator: "kereh",
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "white" },
     { media: "(prefers-color-scheme: dark)", color: "black" },
-  ]
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://mtm-kereh.vercel.app/",
+    title: "Manguni Teknik Multimedia",
+    description: "kami adalah tim kreatif yang berdedikasi tinggi dalam menghadirkan visual dan pengalaman digital yang luar biasa. Sebagai perusahaan yang berfokus pada videography, fotografi, dan pengembangan web, kami menggabungkan keahlian teknis dengan imajinasi artistik untuk menciptakan konten yang menarik dan solusi digital yang inovatif.",
+    siteName: "MTM",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Manguni Teknik Multimedia",
+    description: "kami adalah tim kreatif yang berdedikasi tinggi dalam menghadirkan visual dan pengalaman digital yang luar biasa. Sebagai perusahaan yang berfokus pada videography, fotografi, dan pengembangan web, kami menggabungkan keahlian teknis dengan imajinasi artistik untuk menciptakan konten yang menarik dan solusi digital yang inovatif.",
+    images: [`https://mtm-kereh.vercel.app/og.jpg`],
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
 }
 
 export default function page() {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,15 +7,48 @@ import Footer from '@/components/footer/footer-main'
 import '@/styles/globals.css'
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://mtm-kereh.vercel.app/"),
   title: {
     default: "Manguni Teknik Multimedia",
-    template: "Manguni Teknik Multimedia - %s"
+    template: `%s - Manguni Teknik Multimedia`,
   },
-  description: 'MTM - Manguni Teknik Multimedia',
+  description: "kami adalah tim kreatif yang berdedikasi tinggi dalam menghadirkan visual dan pengalaman digital yang luar biasa. Sebagai perusahaan yang berfokus pada videography, fotografi, dan pengembangan web, kami menggabungkan keahlian teknis dengan imajinasi artistik untuk menciptakan konten yang menarik dan solusi digital yang inovatif.",
+  keywords: [
+    "Photography",
+    "Videography",
+    "Web Developer",
+    "Manguni",
+    "Multimedia",
+    "Web Developer",
+  ],
+  authors: [
+    {
+      name: "kereh",
+      url: "https://github.com/kereh",
+    },
+  ],
+  creator: "kereh",
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "white" },
     { media: "(prefers-color-scheme: dark)", color: "black" },
-  ]
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://mtm-kereh.vercel.app/",
+    title: "Manguni Teknik Multimedia",
+    description: "kami adalah tim kreatif yang berdedikasi tinggi dalam menghadirkan visual dan pengalaman digital yang luar biasa. Sebagai perusahaan yang berfokus pada videography, fotografi, dan pengembangan web, kami menggabungkan keahlian teknis dengan imajinasi artistik untuk menciptakan konten yang menarik dan solusi digital yang inovatif.",
+    siteName: "MTM",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Manguni Teknik Multimedia",
+    description: "kami adalah tim kreatif yang berdedikasi tinggi dalam menghadirkan visual dan pengalaman digital yang luar biasa. Sebagai perusahaan yang berfokus pada videography, fotografi, dan pengembangan web, kami menggabungkan keahlian teknis dengan imajinasi artistik untuk menciptakan konten yang menarik dan solusi digital yang inovatif.",
+    images: [`https://mtm-kereh.vercel.app/og.jpg`],
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
 }
 
 export default function RootLayout({
