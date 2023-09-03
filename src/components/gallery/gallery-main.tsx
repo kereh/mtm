871faf4daec0b409eef76522c0e67587d50616cd
Ronaldo Kereh <kerehchannel@gmail.com>
2023-09-03 12:08:26 +0800

    update: memperbaiki alignment text dari product dan gallery

--- src/components/gallery/gallery-main.tsx
+++ src/components/gallery/gallery-main.tsx
@@ -5,8 +5,8 @@ export default function Gallery() {
   return (
     <section id="gallery" className="w-full flex justify-center items-center my-16">
       <div className="md:max-w-6xl w-full p-6 space-y-6 rounded-lg">
-        <div className="flex flex-col justify-center items-center">
-          <h1 className="text-lg md:text-2xl lg:text-3xl font-semibold">Gallery</h1>
+        <div className="flex flex-col justify-center md:items-center lg:space-y-3">
+          <h1 className="text-lg md:text-2xl lg:text-5xl font-semibold">Gallery</h1>
           <p className="text-muted-foreground">The picture taken is a picture of the portrait we did</p>
         </div>
         <div className="columns-1 md:columns-2 lg:columns-3 gap-4 space-y-4">
--- src/components/products/products-main.tsx
+++ src/components/products/products-main.tsx
@@ -5,10 +5,10 @@ export default function Products() {
   return (
     <section id="product" className="w-full flex justify-center">
       <div className="max-w-6xl w-full p-6 flex flex-col justify-center">
-        <div className="text-center mb-10">
-          <h1 className="text-lg md:text-2xl lg:text-3xl font-semibold">Products</h1>
+        <div className="md:text-center mb-10 lg:space-y-3">
+          <h1 className="text-lg md:text-2xl lg:text-5xl font-semibold">Products</h1>
           <p className="leading-relaxed text-muted-foreground">
-            Explore our products
+            We have some of the best products for you
           </p>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full">
